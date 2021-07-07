00a96c4c25505f125c4dd3c1b6ab8a8e59f6a68d
Anshi Aggarwal <anshi2000aggarwal@gmail.com>
2021-07-07 15:05:21 +0530

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./components/css/styles.css";
+import Data from "./data";
+import OrderList from "./components/OrderList";
+import OrderTotal from "./components/OrderTotal";
+import ToastMessage from "./components/ToastMessage";
 
-function App() {
+export default function App() {
+  const [data, setData] = useState([]);
+  const [toast, setToast] = useState([]);
+
+  //to verify data on inital load
+  useEffect(() => {
+    //check if data already exists
+    const storedData = localStorage.getItem("cart");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    } else {
+      //for first time user
+      const cartData = Data.map((item) => {
+        return { ...item, count: 1 };
+      });
+      setData(cartData);
+      storeData(cartData);
+    }
+  }, []);
+
+  //handle toast functionalities
+  const removeToastItems = (id) => {
+    const newMessages = [];
+    setToast(newMessages);
+  };
+
+  const addToastItem = (id, message) => {
+    const newToast = [...toast, { id, message }];
+    setToast(newToast);
+    removeToastTimeout(id);
+  };
+
+  const removeToastTimeout = (id) => {
+    setTimeout(() => {
+      removeToastItems(id);
+    }, 1000);
+  };
+
+  //util to store data locally
+  const storeData = (items) => {
+    localStorage.setItem("cart", JSON.stringify(items));
+  };
+
+  //cart item functionalities
+  const changeItemCount = (id, value) => {
+    const changedData = data.map((item) => {
+      if (item.id === id) {
+        if (item.count + value >= 0) {
+          item.count += value;
+        }
+      }
+      return item;
+    });
+    setData(changedData);
+    storeData(changedData);
+  };
+
+  const deleteItem = (id, name) => {
+    const changedData = data.filter((item) => item.id !== id);
+    setData(changedData);
+    storeData(changedData);
+    addToastItem(id, `${name} deleted successfully`);
+  };
+
+  // reset data logic
+  const resetData = () => {
+    const cartData = Data.map((item) => {
+      return { ...item, count: 1 };
+    });
+    setData(cartData);
+    storeData(cartData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="centered">
+      <div className="heading">
+        Order Summary
+      </div>
+
+      <div className="order-container">
+        <OrderList
+          data={data}
+          changeItemCount={changeItemCount}
+          deleteItem={deleteItem}
+        />
+        <OrderTotal data={data} />
+      </div>
+
+      <div onClick={resetData} className="reset-btn">Reload
+      </div>
+
+      <ToastMessage message={toast} removeItem={removeToastTimeout} />
     </div>
   );
 }
-
-export default App;
